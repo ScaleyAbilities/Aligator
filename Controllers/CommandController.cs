12cff85ab1a41b1d665e276eaf980ebeb7e4702c
Morgan Zolob <mzolob@gmail.com>
2019-01-24 17:57:21 -0800

    Add skeleton of API project

---
+++ Controllers/CommandController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        // GET test route
+        [HttpGet]
+        public ActionResult<string> Get() 
+        {
+            return "hello world";
+        }
+
+        // POST api/command
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
