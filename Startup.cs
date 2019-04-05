4b70b96286cb35d381622b485f1f8dda99b748be
Morgan Zolob <mzolob@gmail.com>
2019-04-05 14:31:21 -0700

    Allow all CORS requests

--- Startup.cs
+++ Startup.cs
@@ -25,12 +25,23 @@ namespace Asp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("allowAll",
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();;
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("allowAll");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
